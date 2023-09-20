6fb87cf69f150ceb0dd98ef62e9fe44563fd06d8
iramosr <iramosr02@iescastelar.com>
2023-09-20 19:45:36 +0200

    fix issue

--- js0304. Lista de la compra.js
+++ js0304. Lista de la compra.js
@@ -4,9 +4,17 @@ const prompt = require('prompt-sync')();
 let listacompra = [];
 let producto;
 
-while(true){
+while (true) {
     producto = String(prompt('Introdsuce un producto: '));
+    if (listacompra.includes(producto)) {
+        console.log("ya existe");
 
-   
+    } else if (producto == '') {
+        console.log(listacompra);
+        break;
+    } else {
+        listacompra.push(producto)
+        console.log("ya se ha añadido");
+    }
+
 }
-    
