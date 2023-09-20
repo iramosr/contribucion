c7e9b8a0fd6615c69f7f0a05f454b3dab2785a3d
mmane0 <mmanea01@iescastelar.com>
2023-09-20 19:16:28 +0200

    Archivo js

--- js0304. Lista de la compra.js
+++ js0304. Lista de la compra.js
@@ -7,16 +7,6 @@ let producto;
 while(true){
     producto = String(prompt('Introdsuce un producto: '));
 
-    if(listacompra.includes(producto)){
-        console.log("ya existe");
-
-    }else if(producto == ''){
-        console.log(listacompra);
-        break;
-    }else{
-listacompra.push(producto)
-        console.log("ya se ha añadido");
-    }
-
+   
 }
     
